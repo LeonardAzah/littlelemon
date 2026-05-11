5ef62ad9e35225546e564549596ce6c02f10d9bc
LeonardAzah <isongleonard@gmail.com>
2026-05-11 10:36:48 +0100

    add auth classes

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 
@@ -11,31 +12,34 @@ class BookingView(generics.ListCreateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes= [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
 
 class BookingDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
 
 class MenuView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
 
     def get_permissions(self):
         if self.request.method == 'GET':
-            permission_classes = []
-        else:
-            permission_classes = [IsAuthenticated]
+            return []
 
-        return [permission() for permission in permission_classes]
+        return [IsAuthenticated()]
 
 class MenuDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
     def get_permissions(self):
         if self.request.method == 'GET':
-            permission_classes = []
-        else:
-            permission_classes = [IsAuthenticated]
+            return []
 
-        return [permission() for permission in permission_classes]
+        return [IsAuthenticated()]
